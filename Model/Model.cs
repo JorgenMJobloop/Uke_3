f60aeea320bf9763d1eb439cedf2d234382c8bb8
jorgenmjobloop <jorgen@jobloop.no>
2024-10-29 11:05:36 +0100

    inital commit

---
+++ Model/Model.cs
@@ -0,0 +1,19 @@
+public class Model
+{
+    // two fields with getters and setters
+    public string? Name { get; set; }
+    public int Age { get; set; }
+
+    // constructor, that takes two parameters string name & int age
+    public Model(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    // we override the ToString() method, to utilize it in the View class
+    public override string ToString()
+    {
+        return $"Name:{Name} Age:{Age}";
+    }
+    // TODO: verify that this actually does what we want it to, when View is implemented.
+}
